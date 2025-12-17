6bc02d167430b118a359da71596a6b3786bdf753
viclafouch <victor.dlf@outlook.fr>
2025-12-17 11:55:46 +0100

    fix: userjot

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -54,7 +54,7 @@ const RootDocument = ({ children }: { children: React.ReactNode }) => {
       </head>
       <body>
         <ScriptOnce>
-          {`window.$ujq=window.$ujq||[];window.uj=window.uj||new Proxy({},{get:(_,p)=>(...a)=>window.$ujq.push([p,...a])});document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://cdn.userjot.com/sdk/v1/uj.js',async:!0}));`}
+          {`window.$ujq=window.$ujq||[];window.uj=window.uj||new Proxy({},{get:(_,p)=>(...a)=>window.$ujq.push([p,...a])});document.head.appendChild(Object.assign(document.createElement('script'),{src:'https://cdn.userjot.com/sdk/v2/uj.js',type:'module',async:!0}));`}
         </ScriptOnce>
         <ScriptOnce>
           {`(function() {

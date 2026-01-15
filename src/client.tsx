e2ab474ac0b9bc6b860f43271978f95134e81dc7
viclafouch <victor.dlf@outlook.fr>
2026-01-16 00:13:10 +0100

    fix: ignore_dnt

--- src/client.tsx
+++ src/client.tsx
@@ -11,6 +11,10 @@ mixpanel.init('5800e2b9e077ccdaf4cadb637f919c14', {
   // eslint-disable-next-line camelcase
   track_pageview: true,
   autocapture: true,
+  // eslint-disable-next-line camelcase
+  ignore_dnt: true,
+  // eslint-disable-next-line camelcase
+  opt_out_persistence_by_default: false,
   debug: process.env.NODE_ENV === 'development',
   persistence: 'localStorage'
 })

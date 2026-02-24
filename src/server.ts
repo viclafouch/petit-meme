ac8355b73b4b257f79a538121d7a849e5d7f6404
viclafouch <victor.dlf@outlook.fr>
2026-02-24 13:19:05 +0100

    fix: remove www redirection

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,3 @@
-import { clientEnv } from '@/env/client'
 import {
   createStartHandler,
   defaultStreamHandler,
@@ -6,22 +5,7 @@ import {
 } from '@tanstack/react-start/server'
 import { createServerEntry } from '@tanstack/react-start/server-entry'
 
-const canonicalHost = new URL(clientEnv.VITE_SITE_URL).hostname
-
 const customHandler = defineHandlerCallback(async (context) => {
-  const url = new URL(context.request.url)
-
-  if (url.hostname.includes('www.')) {
-    url.hostname = canonicalHost
-
-    return new Response(null, {
-      status: 301,
-      headers: {
-        Location: url.toString()
-      }
-    })
-  }
-
   const response = await defaultStreamHandler(context)
 
   response.headers.set('Cross-Origin-Opener-Policy', 'same-origin')
